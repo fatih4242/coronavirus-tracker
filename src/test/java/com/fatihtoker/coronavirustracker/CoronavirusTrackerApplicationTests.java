ab15093ab9c9090f549f1bb75f8708af58dcadfc
fatih <fatih16@live.nl>
2022-11-05 17:25:35 +0300

    Included Test

--- src/test/java/com/fatihtoker/coronavirustracker/CoronavirusTrackerApplicationTests.java
+++ src/test/java/com/fatihtoker/coronavirustracker/CoronavirusTrackerApplicationTests.java
@@ -1,13 +1,22 @@
 package com.fatihtoker.coronavirustracker;
 
+import com.fatihtoker.coronavirustracker.models.LocationStats;
+import com.fatihtoker.coronavirustracker.services.CoronaVirusDataService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class CoronavirusTrackerApplicationTests {
 
 	@Test
 	void contextLoads() {
+		CoronaVirusDataService coronaVirusDataService = new CoronaVirusDataService();
+		List<LocationStats> allStats = coronaVirusDataService.getAllStats();
+		int totalCases = allStats.stream().mapToInt(stat -> stat.getLatestTotalCases()).sum();
+		int newCases = allStats.stream().mapToInt(stat -> stat.getDiffFromPrevDay()).sum();
+		System.out.println(totalCases +"-"+ newCases);
 	}
 
 }
